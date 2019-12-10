b75e7293795e5a9f7cf26a8bb44fcf65b8209ded
torinberger <torinberger@torinberger.com>
2019-12-10 19:34:08 +1100

    Created

---
+++ index.js
@@ -0,0 +1,55 @@
+
+const axios = require('axios');
+
+module.exports = (ip, port, name, interval) => {
+  return {
+    online: false,
+    deploy() {
+      let time = new Date()
+      this.online = false
+      axios
+        .post(`http://${ip}:${port}/ping`, {
+          name: name,
+          deploy: `[${String(time).substr(0, 10)}] ${name} Deployed!`
+        })
+        .then((res) => {
+          console.log('[Helper Client] Deploy Report Sent!');
+        })
+        .catch(console.log)
+      this.delayInterval(interval)
+    },
+    delayInterval(interval) {
+      let self = this
+      setTimeout(function () {
+        if (self.online) {
+          axios
+            .post(`http://${ip}:${port}/ping`, {
+              name: name
+            })
+            .then((res) => {})
+            .catch(console.log)
+        }
+        self.delayInterval(interval)
+      }, interval);
+    },
+    catch(err) {
+      axios
+        .post(`http://${ip}:${port}/ping`, {
+          name: name,
+          err: String(err)
+        })
+        .then((res) => {
+          console.log('[Helper Client] Error Report Sent!');
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+    },
+    goOffline() {
+      this.online = false
+    },
+    goOnline() {
+      this.online = true
+    }
+  }
+}
